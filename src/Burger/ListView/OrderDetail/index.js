36678fe1bdf63a39cc6dd04e65206530754cc0d7
bhupendra-nitt <bhupendra.nitt@gmail.com>
2020-05-25 23:11:34 +0530

    Add style to list view

--- src/Burger/ListView/OrderDetail/index.js
+++ src/Burger/ListView/OrderDetail/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './_orderDetail.scss';
 import { calculatePriceOfBurger } from '../helper';  
 
 class OrderDetail extends React.PureComponent {
@@ -6,17 +7,54 @@ class OrderDetail extends React.PureComponent {
     const { order } = this.props;
 
     return (
-      <div>
-        <div>
-          Ordered by: {order.name}
-          <div>
+      <div className="order-detail">
+        <div className="order-detail__name">
+          Ordered by: <span>{order.name}</span>
+          <div className="order-detail__list">
             {order.list.map(ele => {
               const price = calculatePriceOfBurger(ele);
-            return <div>{JSON.stringify(ele) }, {price}</div>
+            return <div className="order-detail__list__detail">
+              <div className="order-detail__list__detail__buns">
+                <div className="order-detail__list__detail__buns--label">
+                  Buns
+                </div>
+                <div className="order-detail__list__detail__buns--value">
+                  {ele.buns}
+                </div> 
+              </div>
+              <div className="order-detail__list__detail__salad">
+                <div className="order-detail__list__detail__salad--label">
+                  Salad
+                </div>
+                <div
+                  className="order-detail__list__detail__salad--value"
+                  type="checkbox"
+                >{ele.salad}</div>
+              </div>
+              <div className="order-detail__list__detail__cheese-slice">
+                <div
+                  className="order-detail__list__detail__cheese-slice--label"
+                >
+                  Cheese Slice
+                </div>
+                <div
+                  className="order-detail__list__detail__cheese-slice--value"
+            >{ele.cheeseSlice}</div>
+              </div>
+              <div className="order-detail__list__detail__cutlet">
+                <div
+                  className="order-detail__list__detail__cutlet--label"
+                >
+                  Cutlet
+                </div>
+                <div
+                  className="order-detail__list__detail__cutlet--value"
+            >{ele.cutlet}</div>
+              </div>
+            </div>
             })}
           </div>
         </div>
-
       </div>
     )
   }
--- src/Burger/ListView/index.js
+++ src/Burger/ListView/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './_listView.scss';
 import OrderDetail from './OrderDetail';
 import { calculateTotalSale } from './helper';
 
@@ -9,25 +10,33 @@ class ListView extends React.Component {
       searchString: null
     }
   }
+
   render ( ) {
     const { searchString } = this.state;
     const tempOrders = [];
     return (
-      <div>
-      list view
-      <input onChange={e=> this.setState({searchString: e.target.value})}/>
-      {this.props.orders.map(ele => {
-        if(!searchString) {
-          tempOrders.push(ele);
-          return <OrderDetail order={ele}/>
-        } else {
-          if(ele.name.includes(searchString)) {
-            tempOrders.push(ele);
-            return <OrderDetail order={ele} />
-          }
-        }
-      })}
-        {calculateTotalSale(tempOrders)}
+      <div className="list-view">
+         {this.props.orders.length === 0 ? <div className="list-view__no-orders">No orders</div> :
+         <React.Fragment>
+          <input
+            className="list-view__search-input"
+            onChange={e=> this.setState({searchString: e.target.value})}
+          />
+        
+        { this.props.orders.map(ele => {
+            if(!searchString) {
+              tempOrders.push(ele);
+              return <OrderDetail order={ele}/>
+            } else {
+              if(ele.name.includes(searchString)) {
+                tempOrders.push(ele);
+                return <OrderDetail order={ele} />
+              }
+            }
+          })}
+          <div>Total Amount: {calculateTotalSale(tempOrders)}</div>
+          </React.Fragment>
+       }
       </div>
     )
   }
